85a3bfc0f0658bdbd36a262473cc946f16757245
Giacomo Bresciani <giacomo.bresciani@facile.it>
2018-01-18 18:06:33 +0100

    First commit

---
+++ src/main/kotlin/it/gbresciani/statevalue/StateValue.kt
@@ -0,0 +1,72 @@
+package it.gbresciani.statevalue
+
+/**
+ * Sealed class that represents a value that could be in multiple state depending on whether it is
+ * loading or in error and if a previous value was already loaded. To represent this classification
+ * it is in turn divided into two sub sealed classes, [NoValue] and [WithValue], and it uses two
+ * interfaces, [ErrorState] and [LoadingState].
+ * The full class structure is given below:
+ *
+ * - StateValue
+ *      - NoValue
+ *          - Missing: The value is missing and it is not loading nor in error
+ *          - Loading [LoadingState]: The value is loading and it does not encapsulate a previous loaded value
+ *          - Error [ErrorState]: The value was not loaded because of an error and it does not encapsulate a previous loaded value
+ *      - WithValue: No previous state
+ *          - Loading [LoadingState]: The value is loading and it does encapsulate a previous loaded value
+ *          - Error [ErrorState]: The value was not loaded because of an error but it does encapsulate a previous loaded value
+ *          - Value: The value is loaded
+ *
+ * Cases implementing [ErrorState] could provide an object [E] to provide more info about the error
+ *
+ * Cases implementing [LoadingState] could provide a [LoadingState.Progress] representing the progress
+ * of the loading process from 0 to 100.
+ */
+sealed class StateValue<T> {
+
+    interface ErrorState<out E> {
+        val error: E?
+    }
+
+    interface LoadingState {
+        val progress: Progress?
+
+        data class Progress(val value: Int) {
+            init {
+                if (value !in (0..100)) throw IllegalStateException("Progress value should be a value between 0 and 100.")
+            }
+        }
+    }
+
+    sealed class NoValue<T> : StateValue<T>() {
+        class Missing<T> : NoValue<T>() {
+            override fun equals(other: Any?): Boolean = true
+            override fun hashCode(): Int = super.hashCode()
+        }
+
+        data class Loading<T>(override val progress: LoadingState.Progress? = null) : NoValue<T>(), LoadingState
+        data class Error<T, E>(override val error: E? = null) : NoValue<T>(), ErrorState<E>
+    }
+
+    sealed class WithValue<T> : StateValue<T>() {
+        abstract val value: T
+
+        data class Loading<T>(override val value: T, override val progress: LoadingState.Progress? = null) : WithValue<T>(), LoadingState
+        data class Error<T, E>(override val value: T, override val error: E? = null) : WithValue<T>(), ErrorState<E>
+        data class Value<T>(override val value: T) : WithValue<T>()
+    }
+
+    fun copyToMissing(): StateValue<T> = NoValue.Missing()
+
+    fun copyToLoading(progress: LoadingState.Progress? = null): StateValue<T> = when (this) {
+        is StateValue.NoValue -> NoValue.Loading(progress)
+        is StateValue.WithValue -> WithValue.Loading(value, progress)
+    }
+
+    fun <E> copyToError(error: E? = null): StateValue<T> = when (this) {
+        is StateValue.NoValue -> NoValue.Error(error)
+        is StateValue.WithValue -> WithValue.Error(value, error)
+    }
+
+    fun copyToValue(value: T): StateValue<T> = WithValue.Value(value)
+}
---
+++ src/test/kotlin/it/gbresciani/statevalue/StateValueTest.kt
@@ -0,0 +1,99 @@
+package it.gbresciani.statevalue
+
+import io.kotlintest.properties.Gen
+import io.kotlintest.properties.Gen.Companion.oneOf
+import io.kotlintest.properties.Gen.Companion.string
+import io.kotlintest.properties.forAll
+import io.kotlintest.specs.ShouldSpec
+
+class StateValueTest : ShouldSpec() {
+      init {
+            "StateValue.copyToMissing" {
+                  should("return always a StateValue.NoValue.Missing") {
+                        forAll(StateValueGen.stateValueGen(Gen.int())) { stateValue ->
+                              stateValue.copyToMissing() == StateValue.NoValue.Missing<Int>()
+                        }
+                  }
+            }
+
+            "StateValue.copyToLoading" {
+
+                  "when called on a StateValue.NoValue" {
+                        should("return always a StateValue.NoValue.Loading") {
+                              forAll(StateValueGen.noValueGen<Int>(), StateValueGen.progressGen()) { stateValue, progress ->
+                                    stateValue.copyToLoading(progress) == StateValue.NoValue.Loading<Int>(progress)
+                              }
+                        }
+                  }
+
+                  "when called on a StateValue.WithValue" {
+                        should("return always a StateValue.WithValue.Loading") {
+                              forAll(StateValueGen.withValueGen(Gen.int()), StateValueGen.progressGen()) { stateValue, progress ->
+                                    stateValue.copyToLoading(progress) == StateValue.WithValue.Loading(stateValue.value, progress)
+                              }
+                        }
+                  }
+            }
+
+            "StateValue.copyToError" {
+
+                  "when called on a StateValue.NoValue" {
+                        should("return always a StateValue.NoValue.Error") {
+                              forAll(StateValueGen.noValueGen<Int>(), Gen.string()) { stateValue, error ->
+                                    stateValue.copyToError(error) == StateValue.NoValue.Error<Int, String>(error)
+                              }
+                        }
+                  }
+
+                  "when called on a StateValue.WithValue" {
+                        should("return always a StateValue.WithValue.Error") {
+                              forAll(StateValueGen.withValueGen(Gen.int()), Gen.string()) { stateValue, error ->
+                                    stateValue.copyToError(error) == StateValue.WithValue.Error(stateValue.value, error)
+                              }
+                        }
+                  }
+            }
+
+
+            "StateValue.copyToValue" {
+                  should("return always a StateValue.WithValue.Value") {
+                        forAll(StateValueGen.stateValueGen(Gen.int()), Gen.int()) { stateValue, value ->
+                              stateValue.copyToValue(value) == StateValue.WithValue.Value(value)
+                        }
+                  }
+            }
+      }
+
+      object StateValueGen {
+
+            fun progressGen() = object : Gen<StateValue.LoadingState.Progress> {
+                  override fun generate(): StateValue.LoadingState.Progress =
+                      StateValue.LoadingState.Progress(oneOf((0..100).toList()).generate())
+            }
+
+            fun <T> noValueGen() = object : Gen<StateValue.NoValue<T>> {
+                  override fun generate(): StateValue.NoValue<T> = when (oneOf(listOf(0, 1)).generate()) {
+                        0 -> StateValue.NoValue.Missing()
+                        1 -> StateValue.NoValue.Loading(oneOf(listOf(progressGen().generate(), null)).generate())
+                        else -> StateValue.NoValue.Error(oneOf(listOf(string().generate(), null)).generate())
+                  }
+            }
+
+            fun <T> withValueGen(tGen: Gen<T>) = object : Gen<StateValue.WithValue<T>> {
+                  override fun generate(): StateValue.WithValue<T> = when (oneOf(listOf(0, 1)).generate()) {
+                        0 -> StateValue.WithValue.Loading(tGen.generate(), oneOf(listOf(progressGen().generate(), null)).generate())
+                        1 -> StateValue.WithValue.Error(tGen.generate(), oneOf(listOf(string().generate(), null)).generate())
+                        else -> StateValue.WithValue.Value(tGen.generate())
+                  }
+            }
+
+            fun <T> stateValueGen(tGen: Gen<T>) = object : Gen<StateValue<T>> {
+                  override fun generate(): StateValue<T> = oneOf(listOf(
+                      noValueGen<T>(),
+                      withValueGen(tGen)
+                  )).generate().generate()
+            }
+
+      }
+
+}
