272464f4121dbb3c1241776c42e706dc6032e8e8
Giacomo Bresciani <giacomo.bresciani@facile.it>
2018-01-22 14:53:46 +0100

    Improve Renderers with set functions

--- src/main/kotlin/it/gbresciani/statevalue/StateValueView.kt
+++ src/main/kotlin/it/gbresciani/statevalue/StateValueView.kt
@@ -1,8 +1,7 @@
 package it.gbresciani.statevalue
 
 import android.view.View
-import it.gbresciani.statevalue.StateValue.NoValue
-import it.gbresciani.statevalue.StateValue.WithValue
+import it.gbresciani.statevalue.StateValue.*
 
 interface StateValueRenderer<T> {
       var currentStateValue: StateValue<T>
@@ -10,48 +9,65 @@ interface StateValueRenderer<T> {
 }
 
 interface MissingStateValueView<out MV : View> {
+      val setMissingState: (() -> Unit)?
       val missingView: MV
 }
 
 interface LoadingStateValueView<out LV : View> {
+      val setLoadingState: ((LoadingState.Progress?) -> Unit)?
       val loadingView: LV
 }
 
-interface ErrorStateValueView<out EV : View> {
+interface ErrorStateValueView<out EV : View, in E> {
+      val setErrorState: ((E) -> Unit)?
       val errorView: EV
 }
 
-interface WithValueLoadingStateValueView<out WVLV : View> {
+interface WithValueLoadingStateValueView<out WVLV : View, in T> {
+      val setWithValueLoadingState: ((T, LoadingState.Progress?) -> Unit)?
       val withValueLoadingView: WVLV
 }
 
-interface WithValueErrorStateValueView<out WVEV : View> {
+interface WithValueErrorStateValueView<out WVEV : View, in T, in E> {
+      val setWithValueErrorState: ((T, E) -> Unit)?
       val withValueErrorView: WVEV
 }
 
-interface ValueStateValueView<out VV : View> {
+interface ValueStateValueView<out VV : View, in T> {
+      val setValueState: ((T) -> Unit)?
       val valueView: VV
 }
 
 
-open class SimpleStateValueRenderer<T, out MV : View, out LV : View, out EV : View, out VV : View>(
+open class SimpleStateValueRenderer<T, in E, out MV : View, out LV : View, out EV : View, out VV : View>(
     override val missingView: MV,
     override val loadingView: LV,
     override val errorView: EV,
     override val valueView: VV,
+    override val setMissingState: (() -> Unit)? = null,
+    override val setLoadingState: ((LoadingState.Progress?) -> Unit)? = null,
+    override val setErrorState: ((E) -> Unit)? = null,
+    override val setValueState: ((T) -> Unit)? = null,
     protected val hidingStrategy: HidingStrategy = HidingStrategy.GONE,
     protected val animate: Boolean = true
 ) : StateValueRenderer<T>,
     MissingStateValueView<MV>,
     LoadingStateValueView<LV>,
-    ErrorStateValueView<EV>,
-    ValueStateValueView<VV> {
+    ErrorStateValueView<EV, E>,
+    ValueStateValueView<VV, T> {
 
       override var currentStateValue: StateValue<T> = NoValue.Missing()
 
       override fun render(stateValue: StateValue<T>) {
             if (stateValue == currentStateValue) return
-            
+
+            when (stateValue) {
+                  is NoValue.Missing<T> -> setMissingState?.invoke()
+                  is LoadingState -> setLoadingState?.invoke(stateValue.progress)
+                  is ErrorState<*> -> setErrorState?.invoke(stateValue.error as E)
+                  is WithValue.Value -> setValueState?.invoke(stateValue.value)
+            }
+
             when (stateValue) {
                   is NoValue.Missing -> renderState(
                       show = listOf(missingView),
@@ -84,28 +100,44 @@ open class SimpleStateValueRenderer<T, out MV : View, out LV : View, out EV : Vi
 
                   )
             }
+
             currentStateValue = stateValue
       }
 }
 
-class FullStateValueRenderer<T, out MV : View, out LV : View, out EV : View, out WVLV : View, out WVEV : View, out VV : View>(
+class FullStateValueRenderer<T, in E, out MV : View, out LV : View, out EV : View, out WVLV : View, out WVEV : View, out VV : View>(
     override val missingView: MV,
     override val loadingView: LV,
     override val errorView: EV,
     override val withValueLoadingView: WVLV,
     override val withValueErrorView: WVEV,
     override val valueView: VV,
+    setMissingState: (() -> Unit)? = null,
+    setLoadingState: ((LoadingState.Progress?) -> Unit)? = null,
+    setErrorState: ((E) -> Unit)? = null,
+    override val setWithValueLoadingState: ((T, LoadingState.Progress?) -> Unit)? = null,
+    override val setWithValueErrorState: ((T, E) -> Unit)? = null,
+    setValueState: ((T) -> Unit)? = null,
     hidingStrategy: HidingStrategy = HidingStrategy.GONE,
     animate: Boolean = true
-) : SimpleStateValueRenderer<T, MV, LV, EV, VV>(missingView, loadingView, errorView, valueView, hidingStrategy, animate),
-    WithValueLoadingStateValueView<WVLV>,
-    WithValueErrorStateValueView<WVEV> {
+) : SimpleStateValueRenderer<T, E, MV, LV, EV, VV>(missingView, loadingView, errorView, valueView, setMissingState, setLoadingState, setErrorState, setValueState, hidingStrategy, animate),
+    WithValueLoadingStateValueView<WVLV, T>,
+    WithValueErrorStateValueView<WVEV, T, E> {
 
       override var currentStateValue: StateValue<T> = NoValue.Missing()
 
       override fun render(stateValue: StateValue<T>) {
             if (stateValue == currentStateValue) return
-            
+
+            when (stateValue) {
+                  is NoValue.Missing -> setMissingState?.invoke()
+                  is NoValue.Loading -> setLoadingState?.invoke(stateValue.progress)
+                  is NoValue.Error<*, *>-> setErrorState?.invoke(stateValue.error as E)
+                  is WithValue.Loading -> setWithValueLoadingState?.invoke(stateValue.value, stateValue.progress)
+                  is WithValue.Error<*, *>-> setWithValueErrorState?.invoke(stateValue.value as T, stateValue.error as E)
+                  is WithValue.Value -> setValueState?.invoke(stateValue.value)
+            }
+
             when (stateValue) {
                   is NoValue.Missing -> renderState(
                       show = listOf(missingView),
@@ -149,6 +181,7 @@ class FullStateValueRenderer<T, out MV : View, out LV : View, out EV : View, out
                       animate = animate
 
                   )
+
             }
             currentStateValue = stateValue
       }
